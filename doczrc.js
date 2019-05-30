19aadad70f2b234c1889bbd1ad5a1917ca35d148
zhaozhiwen <zhiwen.zhao@ximalaya.com>
2019-05-30 10:06:30 +0800

    build(.npmrc): 锁定npm下载源 以及发布源
    yarn.lock文件指向了xnpm导致非公司内网无法安装依赖 通过npmrc限制下载源为官方源，通过package.json中的publishConfig控制发布源为xnpm

--- doczrc.js
+++ doczrc.js
@@ -1,24 +1,41 @@
 const path = require('path');
+const { css } = require('docz-plugin-css');
 
 module.exports = {
-  dest: '/site',
-  base: '/dora-ui/',
-  title: 'Dora UI',
-  hashRouter: true,
-  codeSandbox: false,
-  themeConfig: {
-    showPlaygroundEditor: true
+  dest: '/site', // build文件存放目录
+  base: '/dora-ui/', // public path
+  title: 'Dora UI', // 网站名称
+  codeSandbox: false, // 是否开启codeSandbox
+  htmlContext: {
+    // 引入html一些内容
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          href: 'https://codemirror.net/theme/material.css'
+        }
+      ]
+    }
   },
-  typescript: true,
+  themeConfig: {
+    // 主题配置
+    showPlaygroundEditor: true, // 展示Playground编辑器
+    codemirrorTheme: 'material' // 代码主题
+  },
+  plugins: [
+    css({
+      // css处理
+      preprocessor: 'less'
+    })
+  ],
+  typescript: true, // 支持typescript组件
+  propsParser: false, // 是否解析组件props用于官方Props组件展示
   notUseSpecifiers: true,
   filterComponents: files =>
     files.filter(filepath => /components\/.*\/*\.(js|jsx|ts|tsx)$/.test(filepath)),
   modifyBundlerConfig: config => {
-    config.resolve.alias.components = path.resolve(__dirname, './components');
-    config.module.rules.push({
-      test: /\.less$/,
-      use: ['style-loader', 'css-loader', 'less-loader']
-    });
+    // 自定义webpack配置
+    config.resolve.alias.components = path.resolve(__dirname, './components'); // 配置alias
     return config;
   },
   menu: [
