cfd9b2204dcf47c24a8a4afda1a366eb499867f9
zhaozhiwen <zhiwen.zhao@ximalaya.com>
2019-05-30 17:38:28 +0800

    build(doczrc.js): 文档站点生产环境样式处理
    可行方法：组件mdx文件均引入一个全量less，打包时进行命名覆盖。
    原本解法：组件mdx文件中引入对应组件的([component]/style/index.ts)。
    为什么不行：docz build时无法找出ts文件引入的less文件依赖，导致样式失效。
    笨蛋方法：手动引入less文件，并且组件依赖的组件的样式也许需要手动引入，心智负担较重。
    所以新建docs/style/index.less引入全量less文件（组件以及公用样式）用于组件mdx引入，由于docz使用了code-splitting，导致build时会生成多个全量css（诸如[dora-ui.[component].[hash].css），故打包时进行名称覆盖。

--- doczrc.js
+++ doczrc.js
@@ -1,15 +1,47 @@
-const path = require('path');
-const { css } = require('docz-plugin-css');
-const webpackMerge = require('webpack-merge');
 const doczPluginNetlify = require('docz-plugin-netlify');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpackMerge = require('webpack-merge');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const modifyBundlerConfig = (config, dev) => {
+  const styleLoaders = ['css-loader'];
+  styleLoaders.unshift(dev ? 'style-loader' : MiniCssExtractPlugin.loader);
   return webpackMerge(config, {
     resolve: {
       alias: {
         components: path.resolve(__dirname, './components') // 配置alias
       }
-    }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: styleLoaders
+        },
+        { test: /\.less$/, use: [...styleLoaders, 'less-loader'] }
+      ]
+    },
+    optimization: {
+      minimizer: [new OptimizeCSSAssetsPlugin({})]
+    },
+    /**
+     * 每个组件mdx都引入一个全量less 打包时进行命名覆盖
+     * 为什么？
+     * 最优解法为组件mdx中直接引入component/style/index.ts进行样式按需加载 就和我们组件库按需加载一样
+     * 然而 docz build之后 并无法打包ts文件引入的less（dev没问题）
+     * 一个一个手动引入less文件还要考虑到所写组件依赖的组件的样式 心智成本太高了（没错，和组件库按需引入思路一致，所以才有了component/style/index.ts）
+     * 统一写一个全量的less文件（docs/style/index.lesss）专门用于组件mdx引入
+     * 最后打包时又会生成多个全量css文件 dora-ui-[components].[hash].css（因为docz默认存在code splitting）
+     * 所以每个组件mdx只引入一个全量less 同时打包时进行命名覆盖 最终只生成一个全量css文件 每个组件页面都会去请求
+     */
+    plugins: [
+      dev
+        ? () => {}
+        : new MiniCssExtractPlugin({
+            chunkFilename: 'static/css/common.[hash].css'
+          })
+    ]
   });
 };
 
@@ -38,12 +70,7 @@ module.exports = {
   filterComponents: files =>
     files.filter(filepath => /components\/.*\/*\.(js|jsx|ts|tsx)$/.test(filepath)),
   modifyBundlerConfig,
-  plugins: [
-    doczPluginNetlify(),
-    css({
-      preprocessor: 'less'
-    })
-  ],
+  plugins: [doczPluginNetlify()],
   menu: [
     'Introduction', // auto ordered menu "Introduction"
     'Quick Start',
