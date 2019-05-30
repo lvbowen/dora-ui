fe9c782d6df80c9177768045dfae73efd48ab3bc
zhaozhiwen <zhiwen.zhao@ximalaya.com>
2019-05-30 14:40:36 +0800

    chore: docz production mode lost styles

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## [0.1.6](https://github.com/worldzhao/dora-ui/compare/4e54fd6...v0.1.6) (2019-05-29)
+## 0.1.6 (2019-05-30)
 
 
 ### Bug Fixes
@@ -9,7 +9,7 @@
 ### Features
 
 * **icon:** add Icon ([4e54fd6](https://github.com/worldzhao/dora-ui/commit/4e54fd6))
-* **package.json:** react-spring => react-transition-group ([0e40272](https://github.com/worldzhao/dora-ui/commit/0e40272))
+* **package.json:** react-spring => react-transition-group ([e836a8a](https://github.com/worldzhao/dora-ui/commit/e836a8a))
 * **portal:** add basic portal component ([33a6487](https://github.com/worldzhao/dora-ui/commit/33a6487))
 * **toast:** add component toast ([d0081ef](https://github.com/worldzhao/dora-ui/commit/d0081ef))
 
--- doczrc.js
+++ doczrc.js
@@ -1,8 +1,40 @@
 const path = require('path');
-const { css } = require('docz-plugin-css');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpackMerge = require('webpack-merge');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const modifyBundlerConfig = (config, dev) => {
+  const styleLoaders = ['css-loader'];
+  styleLoaders.unshift(dev ? 'style-loader' : MiniCssExtractPlugin.loader);
+  return webpackMerge(config, {
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, './components') // 配置alias
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: styleLoaders
+        },
+        { test: /\.less$/, use: [...styleLoaders, 'less-loader'] }
+      ]
+    },
+    optimization: {
+      minimizer: [new OptimizeCSSAssetsPlugin({})]
+    },
+    plugins: [
+      dev
+        ? () => {}
+        : new MiniCssExtractPlugin({
+            filename: '[name].css'
+          })
+    ]
+  });
+};
 
 module.exports = {
-  dest: '/site', // build文件存放目录
   base: '/dora-ui/', // public path
   title: 'Dora UI', // 网站名称
   codeSandbox: false, // 是否开启codeSandbox
@@ -22,22 +54,12 @@ module.exports = {
     showPlaygroundEditor: true, // 展示Playground编辑器
     codemirrorTheme: 'material' // 代码主题
   },
-  plugins: [
-    css({
-      // css处理
-      preprocessor: 'less'
-    })
-  ],
   typescript: true, // 支持typescript组件
   propsParser: false, // 是否解析组件props用于官方Props组件展示
   notUseSpecifiers: true,
   filterComponents: files =>
     files.filter(filepath => /components\/.*\/*\.(js|jsx|ts|tsx)$/.test(filepath)),
-  modifyBundlerConfig: config => {
-    // 自定义webpack配置
-    config.resolve.alias.components = path.resolve(__dirname, './components'); // 配置alias
-    return config;
-  },
+  modifyBundlerConfig,
   menu: [
     'Introduction', // auto ordered menu "Introduction"
     'Quick Start',
